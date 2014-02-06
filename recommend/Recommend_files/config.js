3970ab0c2e5795423a0af45e8a153ec41adcea44
mikebochenek <mike.bochenek@gmail.com>
2014-02-06 22:44:20 +0100

    recommendme.ch front end only

---
+++ recommend/Recommend_files/config.js
@@ -0,0 +1,17 @@
+﻿CKEDITOR.editorConfig = function(config) {
+	config.uiColor = '#FAFAFA';
+	config.disableNativeSpellChecker = false;
+
+	config.toolbar = [
+			[ 'NewPage', 'Preview' ],
+			[ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Print', 'SpellChecker', 'Scayt' ],
+			[ 'Undo', 'Redo', '-', 'Find', 'Replace', '-', 'SelectAll', 'RemoveFormat' ],
+			[ 'Maximize', 'ShowBlocks', 'Source' ],
+			'/',
+			[ 'Styles', 'Format', 'Font', 'FontSize' ],
+			[ 'TextColor', 'BGColor' ],
+			[ 'Bold', 'Italic', 'Underline', 'Strike' ],
+			[ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote' ],
+			[ 'JustifyLeft', 'JustifyCenter', 'JustifyBlock' ],
+			[ 'Link', 'Image', 'Table', 'Smiley' ] ];
+};
